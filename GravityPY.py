47dd7e1b329f8f6b56b1682b7b86f88e74a695e6
Declan <quaich@users.noreply.github.com>
2017-01-04 01:19:13 +0000

    All core objectives hit! *party popper*
    Changes in this version:
    
    - Collision detection now fully works
    - The bug where planets would just randomly change direction when a new window is open was fixed (caused by background computing when an extra window is open)
    - Debug functions are all removed
    - Added aesthetic only stars in the background to make the program feel more alive.
    
    UNDOCUMENTED CHANGES FROM PREVIOUS VERSIONS:
    - Added colour changer for the planets/trails using tkinters built in cross platform UI
    
    
    Right now the trail drawing is highly inefficient. I will be looking into python module solutions rather than working on my own system.
    
    Main #TODO list is included in the first few lines of the script
    
    Happy new year! 
    
    - Declan

--- GravityPY.py
+++ GravityPY.py
@@ -1,9 +1,10 @@
 #TODO
-#1.Fix the bug where planets "Forget" their varibales.
-#2.Fix collision detection.
-#3.Work on different intergration methods such as RK4 and Verlet.
-#4.Do anything else that was highlighted on my initial objectives.
-#5.Code efficiency/OOP approach to program,
+
+#1.Work on different intergration methods such as RK4 and Verlet.
+#2:Create solar systems to higlight proof of concept.
+#2.Do anything else that was highlighted on my initial objectives.
+#3.Code efficiency/OOP approach to program,
+#4.increase radius of planets as they consume
 
 #setup
 #Importing modules
@@ -43,7 +44,7 @@ ox = 0
 oy = 0
 #2d arrays
 paused = False
-OBJECTS = [["n" for x in range(17)] for y in range(1000)]#(Y,X) #2d array for planet variables
+OBJECTS = [["n" for x in range(17)] for y in range(10)]#(Y,X) #2d array for planet variables
 exclude = [] #temporary mesure
 disco = False
 imported = False
@@ -71,42 +72,69 @@ Collums of OBJECTS:
 def main():
        if currr > 1:
                for mainl in range(0,currr): #for more than one object
-                   if OBJECTS[mainl][0] != "n":
-                       list1 = (OBJECTS[mainl][0],OBJECTS[mainl][1],OBJECTS[mainl][2],OBJECTS[mainl][3])
-                       objectxy = [((OBJECTS[mainl][0]+OBJECTS[mainl][2])/2),((OBJECTS[mainl][1]+OBJECTS[mainl][3])/2)]
-                       calculatedeltaXY(currr,G,objectxy,list1,mainl,exclude)
-                       OBJECTS[mainl][8] = objectxy [0]
-                       OBJECTS[mainl][9] = objectxy [1]
-       if currr == 1: #for an initial object
-              x = ((OBJECTS[0][0]+OBJECTS[0][2])/2)
-              y = ((OBJECTS[0][1]+OBJECTS[0][3])/2)
-              w.move(OBJECTS[0][14],OBJECTS[0][6],OBJECTS[0][7])
-              OBJECTS[0][0],OBJECTS[0][1],OBJECTS[0][2],OBJECTS[0][3] = w.coords(OBJECTS[0][14])
-              nx = ((OBJECTS[0][0]+OBJECTS[0][2])/2)
-              ny = ((OBJECTS[0][1]+OBJECTS[0][3])/2)
-              xy = [x,y]
-              nxy = [nx,ny]
-              if trail == True: drawtrail(xy,nxy,0)
+                   if mainl not in exclude:
+                       x = 0
+                       alone = True
+                       for lines in OBJECTS:
+                           if lines[0] != "n" and lines[0] != "d":
+                                if mainl != x:
+                                    alone = False
+                                    break
+                           x+=1
+                       if alone == True:
+                           print("'ome alone")
+                           solo(mainl)
+                       if OBJECTS[mainl][0] != "n" and OBJECTS[mainl][0] != "d":
+                           list1 = (OBJECTS[mainl][0],OBJECTS[mainl][1],OBJECTS[mainl][2],OBJECTS[mainl][3])
+                           objectxy = [((OBJECTS[mainl][0]+OBJECTS[mainl][2])/2),((OBJECTS[mainl][1]+OBJECTS[mainl][3])/2)]
+                           calculatedeltaXY(currr,G,objectxy,list1,mainl,exclude)
+                           OBJECTS[mainl][8] = objectxy [0]
+                           OBJECTS[mainl][9] = objectxy [1]
 
+       if currr == 1: solo(0)
+
+def solo(no):
+        x = ((OBJECTS[no][0]+OBJECTS[no][2])/2)
+        y = ((OBJECTS[no][1]+OBJECTS[no][3])/2)
+        w.move(OBJECTS[no][14],OBJECTS[no][6],OBJECTS[no][7])
+        OBJECTS[no][0],OBJECTS[no][1],OBJECTS[no][2],OBJECTS[no][3] = w.coords(OBJECTS[no][14])
+        nx = ((OBJECTS[no][0]+OBJECTS[no][2])/2)
+        ny = ((OBJECTS[no][1]+OBJECTS[no][3])/2)
+        xy = [x,y]
+        nxy = [nx,ny]
+        if trail == True: drawtrail(xy,nxy,no)
 def calculatedeltaXY(currr,G,objectxy,list1,mainl,exclude):
         for planets in range(0,currr):
-            if OBJECTS[planets][0] != "n":
-                object2xy = [((OBJECTS[planets][0]+OBJECTS[planets][2])/2),((OBJECTS[planets][1]+OBJECTS[planets][3])/2)]
-                list2 = (OBJECTS[planets][0],OBJECTS[planets][1],OBJECTS[planets][2],OBJECTS[planets][3])
-                if list1 == list2: continue
-                if planets == mainl: continue
-                if planets in exclude: continue
-                if OBJECTS[planets][0] != "n":
-                    ###Calc variables##
-                    ###EULER###
-                    if default.get() == "Euler 8x" or default.get() == "Euler":
-                            if default.get() == "Euler": step = 0
-                            if default.get() == "Euler 4x": step = 4
-                            Euler(objectxy,object2xy,mainl,planets,step,exclude)
-                    if default.get() == "Trapezium": Trapeziumrule(objectxy,object2xy)
-                    if default.get() == "Verlet": xforce,yforce = Verlet(yn,xn,yforce,xforce,force,theta,step,planets)
-                    if default.get() == "Runge-Kutta": Runge()
-                    if default.get() == "RK4": RK4()
+                if OBJECTS[planets][0] != "n" and planets not in exclude:
+                       object2xy = [((OBJECTS[planets][0]+OBJECTS[planets][2])/2),((OBJECTS[planets][1]+OBJECTS[planets][3])/2)]
+                       list2 = (OBJECTS[planets][0],OBJECTS[planets][1],OBJECTS[planets][2],OBJECTS[planets][3])
+                       if list1 == list2: continue
+                       if planets == mainl: continue
+                       ###essentialy the collision detection###
+                       if planets not in exclude and mainl not in exclude:
+                           radius,theta,xn,yn = maths(objectxy,object2xy)
+                           if colide(mainl,planets,radius) == True:
+                                  if OBJECTS[mainl][10] >= OBJECTS[planets][10]:
+                                         tbd = planets
+                                         OBJECTS[mainl][4] += OBJECTS[planets][4]
+                                  else:
+                                         tbd = mainl
+                                         OBJECTS[planets][4] += OBJECTS[mainl][4]
+                                  exclude.append(tbd)
+                                  w.delete(OBJECTS[tbd][14])
+                                  print("test123",OBJECTS[tbd])
+                                  for x in range(0,len(OBJECTS[tbd])): OBJECTS[tbd][x] = "d"
+                           ###Calc variables##
+                           ###EULER###
+                           else:
+                                  if default.get() == "Euler 8x" or default.get() == "Euler":
+                                         if default.get() == "Euler": step = 0
+                                         if default.get() == "Euler 4x": step = 4
+                                         Euler(objectxy,object2xy,mainl,planets,step,exclude)
+                                  if default.get() == "Trapezium": Trapeziumrule(objectxy,object2xy)
+                                  if default.get() == "Verlet": xforce,yforce = Verlet(yn,xn,yforce,xforce,force,theta,step,planets)
+                                  if default.get() == "Runge-Kutta": Runge()
+                                  if default.get() == "RK4": RK4()
 
 def maths(objectxy,object2xy):
         a = int(objectxy[0] - object2xy[0])
@@ -138,9 +166,6 @@ def physics(mainl,planets,objectxy,object2xy,step,exclude):
        currenttime = time.time() - otime
        #print("TIME",currenttime)
        #Resolving (Right) (positive x)
-       if colide(mainl,planets,radius) == True:
-           accelerationx = 0
-           accelerationy = 0
        vx = accelerationx*currenttime / (step+1)
        vy = accelerationy*currenttime / (step+1)
        #Resolving (Down) (positive y)
@@ -149,13 +174,12 @@ def physics(mainl,planets,objectxy,object2xy,step,exclude):
 def Euler(objectxy,object2xy,mainl,planets,step,exclude):
        prevxy = objectxy
        for hop in range(0,step+1):
-              if mainl not in exclude:
-                  vx,vy = physics(mainl,planets,objectxy,object2xy,step,exclude)
-                  OBJECTS[mainl][6] += vx
-                  OBJECTS[mainl][7] += vy
-                  w.move(OBJECTS[mainl][14],OBJECTS[mainl][6],OBJECTS[mainl][7])
-                  OBJECTS[mainl][0],OBJECTS[mainl][1],OBJECTS[mainl][2],OBJECTS[mainl][3] = w.coords(OBJECTS[mainl][14])
-                  objectxy = [((OBJECTS[mainl][0]+OBJECTS[mainl][2])/2),((OBJECTS[mainl][1]+OBJECTS[mainl][3])/2)]
+              vx,vy = physics(mainl,planets,objectxy,object2xy,step,exclude)
+              OBJECTS[mainl][6] += vx
+              OBJECTS[mainl][7] += vy
+              w.move(OBJECTS[mainl][14],OBJECTS[mainl][6],OBJECTS[mainl][7])
+              OBJECTS[mainl][0],OBJECTS[mainl][1],OBJECTS[mainl][2],OBJECTS[mainl][3] = w.coords(OBJECTS[mainl][14])
+              objectxy = [((OBJECTS[mainl][0]+OBJECTS[mainl][2])/2),((OBJECTS[mainl][1]+OBJECTS[mainl][3])/2)]
        if trail == True: drawtrail(prevxy,objectxy,mainl)
        colour = toggle()
        if colour != "White":
@@ -163,7 +187,6 @@ def Euler(objectxy,object2xy,mainl,planets,step,exclude):
               colour = toggle()
               OBJECTS[mainl][5] = colour
        w.update()
-
 def createplanet(rad,mass,x,y,R,G,B,cx,cy):
     global planetcolour
     global currr
@@ -189,33 +212,16 @@ def createplanet(rad,mass,x,y,R,G,B,cx,cy):
     w.lower("oval")
     w.update()
 
-def colide(mainl,planets,radius):  #this "clipping" bullshit doesnt work and makes the program highly inaccurate. THis needs to be reworked into a working collision model.
+def colide(mainl,planets,radius):
     if radius < OBJECTS[mainl][10]:
         w.lower(OBJECTS[planets][14])
-        if radius < 0.1*OBJECTS[mainl][14]:
-            return True
+        if radius < OBJECTS[mainl][14]: return True
     elif radius < OBJECTS[planets][10]:
         w.lower(OBJECTS[mainl][14])
-        if radius < 0.1*OBJECTS[planets][10]: return True
+        if radius < OBJECTS[planets][10]: return True
     else: return False
 
 ###UI Related subroutines###
-
-def debugtoggle():
-    global debug
-    if debug == False:
-        for i in range(1,10):
-            lfill = "red"
-            if i == 5: lfill = "green"
-            w.create_line(100*i,0,100*i,1000,fill=lfill, dash = (4,4),tags="debug")
-            w.create_line(0,100*i,1000,100*i,fill=lfill, dash = (4,4),tags="debug")
-        w.create_line(0, 0, 1000, 1000, fill="red", dash=(4, 4),tags="debug")
-        w.create_line(0, 1000, 1000, 0, fill="red", dash=(4, 4),tags="debug")
-        #curtime = w.create_text(50,100,fill = "White",tags="debug")
-        #fps = w.create_text(50,150,fill = "White",tags="debug")
-    else:
-        w.delete("debug")
-    debug = not debug
 def fml():
 	global disco
 	disco = not disco
@@ -242,16 +248,17 @@ def drawtrail(prevxy,objectxy,mainl):
 
 def playpause():
        global paused
-       if playpause["text"] == "▐▐  ": playpause["text"] = " ► "
-       else: playpause["text"] = "▐▐  "
+       if playp["text"] == "▐▐  ": playp["text"] = " ► "
+       else: playp["text"] = "▐▐  "
        paused = not paused
 
 def clickfunct(event):
        global ox
        global oy
-       ox = event.x
-       oy = event.y
-       w.create_oval(event.x+2,event.y+2,event.x-2,event.y-2,fill="#FF00EA",tags="shot")
+       if event.x < 1000:
+              ox = event.x
+              oy = event.y
+              w.create_oval(event.x+2,event.y+2,event.x-2,event.y-2,fill="#FF00EA",tags="shot")
 
 def motion(event):
        w.delete("shot")
@@ -281,8 +288,9 @@ def toggle():
      else: return("White")
 def getcolour():
     global planetcolour
+    playpause()
     planetcolour = askcolor()
-
+    playpause()
 ###LOAD AND SAVE SYSTEM###
 def load():
        global imported
@@ -293,8 +301,8 @@ def load():
              OBJECTS = lines
              y = 0
              for row in OBJECTS:
-                    for entity in row:
-                           xd = "n"
+                    for x in range(0,len(row)):
+                           OBJECTS[row][x] = "n"
              w.delete("oval","t")
              for x in lines:
                      try: OBJECTS[y][0] = float(OBJECTS[y][0])
@@ -335,11 +343,16 @@ def save():
                      file.write(" ")
               file.write("\n")
 def startoggle(): #make these shift all in one direction at some point
-    for i in range(0,1000):
-        for x in range(0,1): #make this variable
-            ran = random.randint(0,1000)
-            w.create_oval(ran,i,ran,i,outline="White",tags="star")
-
+    if stary["text"] == "Toggle Stars Off":
+        w.delete("star")
+        stary["text"] = "Toggle Stars On"
+    else:
+        for i in range(0,1000):
+            for x in range(0,1): #make this variable
+                ran = random.randint(0,1000)
+                w.create_oval(ran,i,ran,i,outline="White",tags="star")
+        w.lower("star")
+        stary["text"] = "Toggle Stars Off"
 
 
 
@@ -349,25 +362,17 @@ def startoggle(): #make these shift all in one direction at some point
 w.configure(background="Black")
 b1 = w.create_rectangle(1001,0,1205,1000,fill="white")
 
-playpause = Button(master,text="▐▐  ", command=playpause,font=("Helvetica", 12))
-playpause.place(x=1100,y=5,width=30,height=30)
+playp = Button(master,text="▐▐  ", command=playpause,font=("Helvetica", 12))
+playp.place(x=1100,y=5,width=30,height=30)
 
 trailbutton = Button(master, text="Toggle Trail off", command=lambda: trailtoggle(currr))
-trailbutton.place(x=1100,y=100,width=100)
-
-stary = Button(master, text="Toggle Stars",command=startoggle)
-stary.place(x=1100,y=300,width=100)
-
-
-
-
-
+trailbutton.place(x=1090,y=100,width=120)
 
 discotrail = Button(master,command=fml)
 discotrail.place(x=1190,y=0,width=10,height=10)
 
 colourchoose = Button(master,text="Select colour",command=getcolour)
-colourchoose.place(x=1100,y=160,width = 100)
+colourchoose.place(x=1090,y=160,width = 120)
 
 
 loadfunct = Button(master,text="Load file",command=load)
@@ -376,10 +381,11 @@ loadfunct.place(x=1010,y=160,width=80)
 savefunct = Button(master,text="Save file",command=save)
 savefunct.place(x=1010,y=130,width=80)
 
-debuglines = Button(master, text="Toggle Debug", command=debugtoggle)
-debuglines.place(x=1100,y=130,width=100)
-curtime = w.create_text(50,100,fill = "White")
-fps =     w.create_text(50,150,fill = "White")
+stary = Button(master, text="Toggle Stars on", command=startoggle)
+stary.place(x=1090,y=130,width=120)
+
+curtime = w.create_text(50,30,fill = "White")
+fps =     w.create_text(150,30,fill = "White")
 ###Planet specific variables###
 b2 = w.create_rectangle(1020,200,1180,300,fill="Light Grey")
 
